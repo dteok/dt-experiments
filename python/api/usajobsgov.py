0114011a1e88b87e011bbe5660771ad338a41c73
Danny Teok <43955027+dteok@users.noreply.github.com>
2021-09-16 23:13:11 +0100

    fixed 401 authentication error. (#1)
    * fixed 401 authentication error.
    * submitting what's been done so far
    
    Co-authored-by: Danny Teok <dannyteok@gmail.com>

--- python/api/usajobsgov.py
+++ python/api/usajobsgov.py
@@ -1,7 +1,6 @@
 import requests
 import pandas as pd
 import json
-from requests.auth import HTTPBasicAuth
 
 
 with open('../data/input/api_keys/usajobs_gov.json', 'r') as apifile:
@@ -10,14 +9,37 @@ with open('../data/input/api_keys/usajobs_gov.json', 'r') as apifile:
 url = 'https://data.usajobs.gov/api/search?JobCategoryCode=2210'
 user = creds.get('user_agent')
 authkey = creds.get('authorization_key')
+host = 'data.usajobs.gov'
 # print(f"user: {user}, authorization_key: {authorization_key}")
 
-# kwloc = {'Keyword': 'detonation', 'LocationName': ''}
-# response = requests.get('https://data.usajobs.gov/api/search', auth=HTTPBasicAuth(user, authkey), params=kwloc)
+kwloc = {'Keyword': 'detonation', 'LocationName': ''}
+response = requests.get('https://data.usajobs.gov/api/search', headers={
+  "Host": host,
+  "User-Agent": user,
+  "Authorization-Key": authkey}, params=kwloc)
 
-response = requests.get(url,
-  auth=(user, authkey)
-)
+if response.status_code == 200:
+  # print(response.url)
+  print(response.content)
+else:
+  raise ApiError('Get /tasks/ {}'.format(response.status_code))
 
-print(response)
-# print(response.url)
+# print(content.get('SearchResult'))
+# print(content['SearchResult']['SearchResultItems'])
+# print(type(content))
+
+'''
+Compile a search of paygrade, organisation and starting salary of 
+“Data Analyst”, 
+“Data Scientist” and 
+“Data Engineer” jobs on the USA jobs platform. 
+Make sure you understand how to build CodeList queries so you can look up metadata dimensions
+like ‘PayGrade’, ‘JobCategoryCode’, or ‘SecurityClearanceRequired’.
+
+‘PayGrade’                  >> 
+‘JobCategoryCode’           >> https://data.usajobs.gov/api/Search?JobCategoryCode=6501
+‘SecurityClearanceRequired’ >> https://data.usajobs.gov/api/Search?SecurityClearanceRequired=1
+'''
+
+codelist = 'occupationalseries'
+resp = requests.get(f"https://data.usajobs.gov/api/codelist/{codelist}")
