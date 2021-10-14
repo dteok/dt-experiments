df98fa989dce9527b545fc256eee20f60636741d
Danny Teok <dannyteok@gmail.com>
2021-10-15 00:26:01 +0100

    add todo comments, resizeable set to false, and fix shrinking background with .pack()

--- src/clocks/digital-clock.py
+++ src/clocks/digital-clock.py
@@ -3,6 +3,11 @@ from tkinter.ttk import *
 from time import strftime
 
 
+# TODO:
+# To display day of the week -- Saturday
+# To display date   -- 23 September 2021
+
+
 def time():
     """Displays time as `string`"""
 
@@ -25,10 +30,21 @@ def main():
 if __name__ == "__main__":
     root = Tk()
     root.title("Digital Clock")
+    root.geometry("370x100")
+    root.resizable(0, 0)
 
     label = Label(
-        root, font=("ds-digital", 50), background="black", foreground="lightgreen"
+        root,
+        font=("ds-digital", 50, "bold"),
+        background="black",
+        foreground="lightgreen",
+        borderwidth=25,
+        border=25,
     )  # 50 is the font size. change to whatever
     label.pack(anchor="center")  # displays the digital clock in the middle
+    # label.grid(row=2, column=2, pady=5, padx=10)
+    label.pack(
+        anchor="center", fill="both", expand=1
+    )  # resolves expanding/shrinking when second ticks to 01 (narrowest)
 
     main()
